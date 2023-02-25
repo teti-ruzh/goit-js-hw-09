189b787e67fe8d6fd375bf680b5ecf6a6962511d
teti-ruzh <113991436+teti-ruzh@users.noreply.github.com>
2023-02-25 19:57:02 +0000

    Deploying to gh-pages from @ teti-ruzh/goit-js-hw-09@4906f095ffa409e0e614ac2030efe2dca03dcc9f 🚀

---
+++ 01-color-switcher.0d888660.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");e.disabled=!0;var a=null;t.addEventListener("click",(function(d){a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.0d888660.js.map
---
+++ 01-color-switcher.4c64b4d1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0;let d=null;e.addEventListener("click",(function(a){d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,e.disabled=!0,t.disabled=!1}),1e3)})),t.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.4c64b4d1.js.map
